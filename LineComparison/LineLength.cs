7dd45620c5b4141d2f7d38e8791e9001cbd4983b
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-02-03 03:08:53 +0530

    Calculating length of line

---
+++ LineComparison/LineLength.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class LineLength
+    {
+        public void CalcuLength()
+        {
+            Console.WriteLine("Enter X co-ordinate ");
+            int x1, x2;
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a y co-oordinate");
+            int y1, y2;
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Line Length " + length);
+        }
+    }
+}
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LineComparison;
+
+Console.WriteLine("Hello, Welcome...! to Line Comparison Problem");
+LineLength lineLength=new LineLength();
+lineLength.CalcuLength();
